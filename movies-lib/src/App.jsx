ec436d0dceda4586b3f65f4e3e3d6c4a04aedc9f
Carlos Eduardo Vieira de Souza <carloseduardovieiradesouza@gmail.com>
2023-07-26 12:40:52 -0300

    Finishing Project

---
+++ movies-lib/src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ movies-lib/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import "../components/Navbar.css";
+
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          {" "}
+          <BiCameraMovie /> MoviesLib
+        </Link>
+      </h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
